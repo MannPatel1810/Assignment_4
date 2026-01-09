ed53c5964921bcb99e2a0b7dfe560e13612f3c92
MannPatel1810 <mannp1639@gmail.com>
2026-01-09 19:38:21 +0530

    Add files via upload

--- Assignment 4.2.py
+++ Assignment 4.2.py
@@ -1,9 +1,6 @@
 # Task 2: Write and Append Data to a File
 
-# Problem Statement: Write a Python program that:
-# 1.   Takes user input and writes it to a file named output.txt.
-# 2.   Appends additional data to the same file.
-# 3.   Reads and displays the final content of the file.
+
 
 # Step 1: Take user input and write it to output.txt
 user_input = input("Enter text to write into the file: ")
